2c83bceea7470672adf583ad7243a014102b7c3b
Osvaldo Ramos <osvaldo3378@outlook.com>
2020-10-16 21:03:57 -0300

    implementacao parcial para AceitarConvite

--- CompraAi/CompraAi.Dominio/Usuario.cs
+++ CompraAi/CompraAi.Dominio/Usuario.cs
@@ -1,6 +1,8 @@
 ﻿using CompraAi.Dominio.Validacoes;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CompraAi.Dominio
 {
